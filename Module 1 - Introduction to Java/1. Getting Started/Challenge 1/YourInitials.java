425bbb291db9763b3f97f847bcbc9e9463444156
Peter Hardy <peterphardy@gmail.com>
2023-01-15 22:19:42 -0600

    Adding Module 1 folder to track progress

---
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("PPPP   H   H");
+         System.out.println("P   P  H   H");
+         System.out.println("P   P  H   H");
+         System.out.println("PPPP   HHHHH");
+         System.out.println("P      H   H");
+         System.out.println("P      H   H");
+         System.out.println("P      H   H");
+    }
+}
