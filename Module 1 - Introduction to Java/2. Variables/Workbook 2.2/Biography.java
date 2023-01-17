79685854c67b94b08e3922d6ea045fa816c0ffec
Peter Hardy <peterphardy@gmail.com>
2023-01-16 22:59:55 -0600

    Workbook 2.2 complete

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -4,19 +4,19 @@ public class Biography {
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
 
 
-        // name (text)
-        // age (whole number)
-        // country (text)
-        // sport (text)
-        // hours (whole number)
-        // game (text)
-        // subject (text)
-        // grade (character)
+        String name = "Peter";
+        int age = 33;
+        String country = "United States of America";
+        String sport = "skateboarding";
+        int hours = 3;
+        String game = "hide and go seek";
+        String subject = "history";
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from the " + country + ".");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored a " + grade + "+!");
 
         // Compare your result to what's on Learn the Part.
     }
