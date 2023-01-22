261e0e0cbfd173a57de8b03d5c95a7bf5eb91a2f
Peter Hardy <peterphardy@gmail.com>
2023-01-21 21:07:12 -0600

    Refactored with less lines

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.4/Meeting.java
@@ -8,15 +8,10 @@ public class Meeting {
         double refunds = 688.78; 
         double shipping = 1233.57; 
 
-        int roundedSales = (int)sales;
-        int roundedProfit = (int)profit;
-        int roundedRefunds = (int)refunds;
-        int roundedShipping = (int)shipping;
-
-        System.out.println("This month, we made $" + roundedSales + " in sales");
-        System.out.println("Factoring in costs, we made $" + roundedProfit + " in profit");
-        System.out.println("The refunds are at a low $" + roundedRefunds + ". This is a good sign!");
-        System.out.println("Shipping costs were high. We paid $" + roundedShipping + " in shipping");
+        System.out.println("This month, we made $" + (int)sales + " in sales");
+        System.out.println("Factoring in costs, we made $" + (int)profit + " in profit");
+        System.out.println("The refunds are at a low $" + (int)refunds + ". This is a good sign!");
+        System.out.println("Shipping costs were high. We paid $" + (int)shipping + " in shipping");
 
     }
 }
