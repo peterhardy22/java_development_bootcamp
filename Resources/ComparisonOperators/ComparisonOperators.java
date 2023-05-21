383926cb990da89a20051537ee8d01c6c4a8d83b
Peter Hardy <peterphardy@gmail.com>
2023-05-20 22:39:46 -0500

    Less than <

--- Resources/ComparisonOperators/ComparisonOperators.java
+++ Resources/ComparisonOperators/ComparisonOperators.java
@@ -4,11 +4,17 @@ public class ComparisonOperators {
 
     public static void main(String[] args) {
         
-        int chemistryGrade = 95;
-        int biologyGrade = 75;
+        int chemistryGrade = 75;
+        int biologyGrade = 95;
         System.out.println("Me: hi Java, did I score better in biology?");
         System.out.println("Java: " + (biologyGrade > chemistryGrade) + "\n");
 
+        double sales = 37.55;
+        double costs = 5.55;
+        System.out.println("Me: Hi java, did we make money?");
+        System.out.println("Java: " + (sales < costs) + "\n");
+
+        
     }
     
 }
