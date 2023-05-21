e4d3c491c4c3d29b59c3d8211bcbcc9cb766e3e4
Peter Hardy <peterphardy@gmail.com>
2023-05-20 22:43:40 -0500

    Greater than or equal to >=

--- Resources/ComparisonOperators/ComparisonOperators.java
+++ Resources/ComparisonOperators/ComparisonOperators.java
@@ -6,7 +6,7 @@ public class ComparisonOperators {
         
         int chemistryGrade = 75;
         int biologyGrade = 95;
-        System.out.println("Me: hi Java, did I score better in biology?");
+        System.out.println("Me: Hi Java, did I score better in biology?");
         System.out.println("Java: " + (biologyGrade > chemistryGrade) + "\n");
 
         double sales = 37.55;
@@ -14,7 +14,12 @@ public class ComparisonOperators {
         System.out.println("Me: Hi java, did we make money?");
         System.out.println("Java: " + (sales < costs) + "\n");
 
-        
+        int age = 40;
+        int retirementAge = 62; 
+        System.err.println("Me: Hi Java, am I old enough to retire?");
+        System.out.println("Java: " + (age >= retirementAge) + "\n");
+
+
     }
     
 }
