c4000c49cf3f333071c99ef4f5ae5e13802dd47a
Peter Hardy <peterphardy@gmail.com>
2023-05-20 22:35:48 -0500

    Learning comparison operators

---
+++ Resources/ComparisonOperators/ComparisonOperators.java
@@ -0,0 +1,14 @@
+package Resources.ComparisonOperators;
+
+public class ComparisonOperators {
+
+    public static void main(String[] args) {
+        
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        System.out.println("Me: hi Java, did I score better in biology?");
+        System.out.println("Java: " + (biologyGrade > chemistryGrade) + "\n");
+
+    }
+    
+}
