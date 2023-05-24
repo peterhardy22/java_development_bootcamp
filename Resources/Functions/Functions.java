735b87b314ec15427e37fd757a28d9ee13e57da5
Peter Hardy <peterphardy@gmail.com>
2023-05-23 22:18:03 -0500

    Functions lecture

---
+++ Resources/Functions/Functions.java
@@ -0,0 +1,19 @@
+public class Functions {
+
+    public static void main(String[] args) {
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+
+    }
+
+    public static void singChorus() {
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on the good times");
+        System.out.println("Don't blame it on the boogie\n");
+    }
+
+}
