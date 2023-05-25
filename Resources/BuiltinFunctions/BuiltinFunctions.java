ad473767c702099a28264402b7e950792d7a2fe6
Peter Hardy <peterphardy@gmail.com>
2023-05-24 21:55:15 -0500

    Examples

--- Resources/BuiltinFunctions/BuiltinFunctions.java
+++ Resources/BuiltinFunctions/BuiltinFunctions.java
@@ -7,6 +7,11 @@ public class BuiltinFunctions {
         System.out.println("sunny".length());
         System.out.println("sunny".toUpperCase());
         System.out.println(Math.sqrt(9));
+        System.out.println("Java programming".startsWith("Java"));
+
+        String original = "Java is fun!";
+        String updatedString = original.replace('v', 'b');
+        System.out.println(updatedString);
 
     }
     
