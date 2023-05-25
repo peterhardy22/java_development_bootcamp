1b1520a7f56630d71e0358b2f60bc710f1ffc586
Peter Hardy <peterphardy@gmail.com>
2023-05-24 21:38:45 -0500

    Builtin functions lecture

---
+++ Resources/BuiltinFunctions/BuiltinFunctions.java
@@ -0,0 +1,13 @@
+package Resources.BuiltinFunctions;
+
+public class BuiltinFunctions {
+
+    public static void main(String[] args) {
+        
+        System.out.println("sunny".length());
+        System.out.println("sunny".toUpperCase());
+        System.out.println(Math.sqrt(9));
+
+    }
+    
+}
