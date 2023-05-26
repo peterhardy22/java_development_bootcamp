68cd1a474d2019be32f796e65ce99c5aa6630ef0
Peter Hardy <peterphardy@gmail.com>
2023-05-25 20:22:24 -0500

    For loop lecture

---
+++ Resources/ForLoops/ForLoops.java
@@ -0,0 +1,29 @@
+package Resources.ForLoops;
+
+public class ForLoops {
+
+    public static void main(String[] args) {
+        
+    for (int number = 1; number < 11; number++) {
+        System.out.println(number);
+    }
+
+    for (int number = 0; number < 5; number++) {
+        System.out.println("Hello, erybody!");
+    }
+
+    for (int number = 1; number < 11; number++) {
+        System.out.println(number * 5);
+    }
+
+    for (int number = 10; number > 0; number--) {
+        System.out.println(number);
+    }
+
+    for (int number = 0; number < 19; number+=2) {
+        System.out.println(number);
+    }
+
+    }
+    
+}
