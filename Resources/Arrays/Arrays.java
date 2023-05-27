7a7de99070e2f20360c43711cd87e8e3ddaeeeff
Peter Hardy <peterphardy@gmail.com>
2023-05-27 14:26:33 -0500

    Arrays lecture

---
+++ Resources/Arrays/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+     
+        int[] integers = {1, 2, 3};
+        String[] cities = {"Chicago", "Oakland", "San Franccisco"};
+
+        System.out.println(cities[0]);
+
+    }
+
+}
