c94445b58a90ffe7cb5125cf003a7304b88ded18
Peter Hardy <peterphardy@gmail.com>
2023-05-29 13:25:33 -0500

    Workbook 6.2 completed

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
@@ -1,10 +1,13 @@
 public class Exams {
     public static void main(String[] args) {
+        String[] students = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
         
         // Instructions for this workbook are on Learn the Part (Workbook 6.2).
         System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
         
-        System.out.println("<student at index i>, you will take seat <i>");
+        for (int index = 0; index < students.length; index++) {
+            System.out.println(students[index] + ", you will take seat " + index);
+        }
         
         // Compare your result to what's on Learn the Part.
 
