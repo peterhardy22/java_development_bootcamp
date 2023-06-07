08b38dead3ab7c0c9da64a513e8107f41300de3a
Peter Hardy <peterphardy@gmail.com>
2023-06-06 21:51:34 -0500

    2D array lecture

---
+++ Resources/TDarrays/TDarrays.java
@@ -0,0 +1,16 @@
+package Resources.TDarrays;
+
+import java.util.Arrays;
+
+public class TDarrays {
+    
+    public static void main(String[] args) {
+        
+        int[][] grades = new int[3][4];
+        System.out.println("\tHarry: " + Arrays.toString(grades[0]));
+        System.out.println("\tRon: " + Arrays.toString(grades[1]));
+        System.out.println("\tHermione: " + Arrays.toString(grades[2]));
+
+    }
+    
+}
