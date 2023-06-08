895dee3684538a8a758eb7e525d6395300fb2d27
Peter Hardy <peterphardy@gmail.com>
2023-06-07 22:55:18 -0500

    Looping 2D arrays lecture

--- Resources/TDarrays/TDarrays.java
+++ Resources/TDarrays/TDarrays.java
@@ -11,23 +11,17 @@ public class TDarrays {
                         {65, 64, 61, 67}, 
                         {95, 98, 99, 100}};
 
-        // grades[0][0] = 72;
-        // grades[0][1] = 74;
-        // grades[0][2] = 78;
-        // grades[0][3] = 76;
-        // grades[1][0] = 65;
-        // grades[1][1] = 64;
-        // grades[1][2] = 61;
-        // grades[1][3] = 67;
-        // grades[2][0] = 95;
-        // grades[2][1] = 98;
-        // grades[2][2] = 99;
-        // grades[2][3] = 100;
-
-        System.out.println("\tHarry: " + Arrays.toString(grades[0]));
-        System.out.println("\tRon: " + Arrays.toString(grades[1]));
-        System.out.println("\tHermione: " + Arrays.toString(grades[2]));
-
+        for (int index = 0; index < grades.length; index++) {
+            switch (index) {
+                case 0: System.out.print("\tHarry: "); break;
+                case 1: System.out.print("\tRon: "); break;
+                case 2: System.out.print("\tHermione: "); break;
+            }
+            for (int j = 0; j < grades[index].length; j++) {
+                System.out.print(grades[index][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
     
 }
