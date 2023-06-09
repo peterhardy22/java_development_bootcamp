d415ceef64a641b3b1308d3c71d19a3cdf33122d
Peter Hardy <peterphardy@gmail.com>
2023-06-08 22:28:22 -0500

    Updates

--- Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
+++ Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
@@ -8,46 +8,36 @@ public class Javapedia {
         //Task 1 – Ask the user: how many historical figures will you register?
         //       – Store the value.
         int figureAmount = scan.nextInt();
-        scan.nextLine();
         
         //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
         String[][] database = new String[figureAmount][3];
 
-
+        scan.nextLine();
         //Task 3 - Collect user values for each row.
         for (int i = 0; i < database.length; i++) {
             System.out.println("\n\tFigure " + (i+1)); 
             System.out.print("\t - Name: ");
-            String name = scan.nextLine();
-            database[i][0] = name;
-            
+            database[i][0] = scan.nextLine();
             System.out.print("\t - Date of birth: ");
-            String birthday = scan.nextLine();
-            database[i][1] = birthday;
-
+            database[i][1] = scan.nextLine();
             System.out.print("\t - Occupation: ");
-            String occupation = scan.nextLine(); 
-            database[i][2] = occupation;
-
+            database[i][2] = scan.nextLine(); 
             System.out.print("\n");
         }
 
         //Task 4: call print2DArray. 
         System.out.println("These are the values you stored:"); 
-
         print2DArray(database);
 
         //Task 5: Let the user search the database by name. 
         System.out.print("\nWho do you want information on? ");
-
         String choice = scan.nextLine();
+        System.out.print("\n");
         for (int i = 0; i < database.length; i++) {
-            for (int j = 0; j < database[i].length; j++) {
-                if (choice.equals(database[i][j])) {
-                System.out.println("\n\t - Name: " + database[i][0]);
-                System.out.println("\t - Date of birth: " + database[i][1]);
-                System.out.println("\t - Occupation: " + database[i][2]);
-                }
+            if (choice.equals(database[i][0])) {
+            System.out.println("\t - Name: " + database[i][0]);
+            System.out.println("\t - Date of birth: " + database[i][1]);
+            System.out.println("\t - Occupation: " + database[i][2]);
             }
         }      
         
@@ -56,10 +46,10 @@ public class Javapedia {
 
     public static void print2DArray(String[][] array) {
         for (int i = 0; i < array.length; i++) {
+            System.out.println("\t");
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
-            System.out.print("\n");
         }
     }
 }
