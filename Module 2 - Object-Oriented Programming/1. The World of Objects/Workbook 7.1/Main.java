efacea9a4535e3aec91b14256fd50b985e2db874
Peter Hardy <peterphardy@gmail.com>
2023-08-21 14:53:30 -0500

    Started new exercise

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Workbook 7.1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Person peter = new Person();
+        person.name = "Peter Hardy";
+        person.nationality = "American";
+        person.dateOfBirth = // a String
+        person.passport = // Array that stores: {person.name, person.nationality, person.dateOfBirth}
+        person.seatNumber = // an Integer
+    }
+}
