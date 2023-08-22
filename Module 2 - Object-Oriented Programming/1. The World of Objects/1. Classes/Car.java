27c0b272c03c249b2fb5d9168acee5386179d77a
Peter Hardy <peterphardy@gmail.com>
2023-08-21 21:58:29 -0500

    Constructors make initialization easier code wise

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/1. Classes/Car.java
@@ -0,0 +1,6 @@
+public class Car {
+    public String make;
+    public double price;
+    public int year;
+    public String color;
+}
---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/2. Constructors/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public String make;
+    public int price;
+    public int year;
+    public String color;
+
+    public Car(String make, int price, int year, String color) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+}
