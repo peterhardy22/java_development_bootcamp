137a6a9ec0354191c9a688d7ffd2d93a3b8f4de4
Peter Hardy <peterphardy@gmail.com>
2023-08-22 21:24:49 -0500

    Completed getter method lesson

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/3. Getters/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+public static void main(String[] args) {
+    Car myCar = new Car(100);
+    int fuel = myCar.getFuelLevel();
+
+    System.out.println("The fuel level for this car is " + fuel);
+}
+
+}
