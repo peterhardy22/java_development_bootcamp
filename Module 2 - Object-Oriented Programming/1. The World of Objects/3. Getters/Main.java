68cf74dee2599e41cfc2898ce3add98e243d405f
Peter Hardy <peterphardy@gmail.com>
2023-08-22 21:46:11 -0500

    More getter method practice

--- Module 2 - Object-Oriented Programming/1. The World of Objects/3. Getters/Main.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/3. Getters/Main.java
@@ -2,8 +2,9 @@ public class Main {
     
 public static void main(String[] args) {
     Car myCar = new Car(100);
-    int fuel = myCar.getFuelLevel();
+    System.out.println("The fuel level for this car is " + myCar.getFuelLevel());
 
+    int fuel = myCar.getFuelLevel();
     System.out.println("The fuel level for this car is " + fuel);
 }
 
