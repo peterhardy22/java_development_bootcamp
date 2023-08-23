837d5e9745a6b19a2a7a9a9e38b98815ec09be38
Peter Hardy <peterphardy@gmail.com>
2023-08-22 21:24:27 -0500

    Car class has a getter method

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/3. Getters/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private int fuelLevel;
+
+    public Car(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getFuelLevel() {
+        return this.fuelLevel;
+    }
+}
