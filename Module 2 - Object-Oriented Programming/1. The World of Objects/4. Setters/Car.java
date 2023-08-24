675f2bd6e3472d00f2a04d20bffe3124acdb0540
Peter Hardy <peterphardy@gmail.com>
2023-08-23 22:20:00 -0500

    Learning how to use setters

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/4. Setters/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    private int fuelLevel;
+
+    public Car(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getFuelLevel() {
+        return this.fuelLevel;
+    }
+
+    public void setFuelLevel(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+}
