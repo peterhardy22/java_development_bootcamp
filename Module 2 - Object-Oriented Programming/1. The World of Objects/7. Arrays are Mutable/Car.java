b7cb417ba8e9a34569641f699d401357981ddb96
Peter Hardy <peterphardy@gmail.com>
2023-08-28 21:40:45 -0500

    Added parts field to Car class

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/7. Arrays are Mutable/Car.java
@@ -0,0 +1,69 @@
+public class Car {
+    public String make;
+    public double price;
+    public int year;
+    public String color;
+    public String[] parts;
+
+    public Car(String make, double price, int year, String color, String[] parts) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+        this.parts = parts;
+    }
+
+    public Car(Car source) {
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+        this.parts = source.parts;
+    }
+
+    public String[] getParts() {
+        return this.parts;
+    }
+
+    public void setParts(String[] parts) {
+        this.parts = parts;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void drive() {
+        System.out.println("\nYou bought the beautiful " + this.year + " " + this.color + " " + this.make + " for " + this.price + ".");
+        System.out.println("Please drive your car to the nearest exit.\n");
+    }
+
+}
