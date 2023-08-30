67ee78ce418d511e628b854ea63602a77329cc89
Peter Hardy <peterphardy@gmail.com>
2023-08-29 22:35:30 -0500

    Adding parts field ad hoc

--- Module 2 - Object-Oriented Programming/1. The World of Objects/7. Arrays are Mutable/Main.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/7. Arrays are Mutable/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     
     public static void main(String[] args) {
-        String[] spareParts = new String[] {"Tires", "Keys"};
-        
-        Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue", spareParts);
-        
-        spareParts[0] = "Filter";
-
+        Car nissan = new Car("Nissan", 10000, 2020, "Green", new String[] {"Tires", "Keys"});
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", new String[] {"Tires", "Keys"});
         Car nissan2 = new Car(nissan);
         
         nissan2.setColor("Yellow");
