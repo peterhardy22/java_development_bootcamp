7cc5c5bbac14919579199c4bacbc5b2d756a89e6
Peter Hardy <peterphardy@gmail.com>
2023-08-31 22:49:53 -0500

    Up to Task 3 has been completed

---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Workbook 7.6/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+        
+        person.chooseSeat();
+
+        System.out.println("Name: " + person.getName() + "\n" +
+        "Nationality: " + person.getNationality() + "\n" + 
+        "Date of Birth: " + person.getDateOfBirth() + "\n" + 
+        "Seat Number: " + person.getSeatNumber() + "\n");
+
+        if (person.applyPassport()) {
+            System.out.println("Congratulations " + person.getName() + ". Your passport was approved!");
+        } else {
+            System.out.println("We are sorry " + person.getName() + ". We cannot process your application.");
+        }
+
+        System.out.println("Name: " + person.getName() + "\n" + 
+        "Nationality: " + person.getNationality() + "\n" + 
+        "Date of Birth: " + person.getDateOfBirth() + "\n" +
+        "Seat Number: " + person.getSeatNumber() + "\n" + "Passport: " +
+        Arrays.toString(person.getPassport()) + "\n");
+
+
+    }
+
+}
---
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Workbook 7.6/Person.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Person {
+    
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
+    private String[] passport;
+    private int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name; 
+        this.nationality = nationality; 
+        this.dateOfBirth = dateOfBirth; 
+        this.seatNumber = seatNumber;
+        this.passport = new String[3]; 
+    }
+
+    public Person(Person source) {
+        this.name = source.name;
+        this.nationality = source.nationality;
+        this.dateOfBirth = source.dateOfBirth;
+        this.seatNumber = source.seatNumber;
+    }
+
+    public boolean applyPassport() {
+        int number = (int) (Math.random() * 2);
+        return number == 1;
+    }
+
+    public void chooseSeat() {
+        seatNumber = (int) (Math.random() * 11 + 1);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String[] getPassport() {
+        return Arrays.copyOf(this.passport, this.passport.length);
+    }
+}
+
