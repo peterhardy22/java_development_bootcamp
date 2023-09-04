c637ec9fe46b470f3b65b592d38df87f6021add1
Peter Hardy <peterphardy@gmail.com>
2023-09-03 22:17:24 -0500

    Shows the parking lot

--- Module 2 - Object-Oriented Programming/1. The World of Objects/8. Multiple Objects/Dealership.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/8. Multiple Objects/Dealership.java
@@ -8,5 +8,15 @@ public class Dealership {
             this.cars[i] = new Car(cars[i]);
         }
     }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.cars.length; i++) {
+            temp += "Parking Spot: " + i + "\n";
+            String carDescription = this.cars[i].toString();
+            temp += carDescription + "\n";
+        }
+        return temp;
+    }
     
 }
