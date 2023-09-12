7fb9367015b74895457cb1f5b9806a40c94c27c5
Peter Hardy <peterphardy@gmail.com>
2023-09-11 22:03:26 -0500

    Task 2 complete

--- Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/Contact.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/Contact.java
@@ -4,4 +4,10 @@ public class Contact {
     private String birthDate;
     private int age;
 
+    public Contact(String name, String phoneNumber, String birthDate, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;             
+        this.age = age;
+    }
 }
