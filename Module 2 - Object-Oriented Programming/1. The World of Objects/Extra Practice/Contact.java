6d17e3b6bd796f649e2ca627216c26834b90f343
Peter Hardy <peterphardy@gmail.com>
2023-09-13 22:01:38 -0500

    Task 3 completed

--- Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/Contact.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/Contact.java
@@ -1,17 +1,32 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Contact {
     private String name;
     private String phoneNumber;
     private String birthDate;
+    private int age;
 
     public Contact(String name, String phoneNumber, String birthDate, int age) {
         this.name = name;
         this.phoneNumber = phoneNumber;
-        this.birthDate = birthDate;             
+        this.birthDate = birthDate; 
+        this.age = age;     
     }
 
     public Contact(Contact source) {
         this.name = source.name;
         this.phoneNumber = source.phoneNumber;
-        this.birthDate = source.birthDate;             
+        this.birthDate = source.birthDate;  
+        this.age = toAge(birthDate);        
     }
+
+
+    public int toAge(String birthDate) {
+        LocalDate dateOfBirth = LocalDate.parse(birthDate);
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(today, dateOfBirth);
+        return period.getYears();    
+    }
+    
 }
