84bcc7f7a727fd323a25083360a1d92fc28ab58a
Peter Hardy <peterphardy@gmail.com>
2023-09-15 21:45:27 -0500

    Task 2 completed

--- Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/ContactManager.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/ContactManager.java
@@ -1,5 +1,12 @@
 public class ContactManager {
     private Contact[] contacts;
     
+    public ContactManager(Contact[] contacts) {
+        this.contacts = new Contact[contacts.length];
+        for (int i = 0; i < this.contacts.length; i++) {
+            this.contacts[i] = new Contact(contacts[i]);
+        }
 
+    }
+    
 }
