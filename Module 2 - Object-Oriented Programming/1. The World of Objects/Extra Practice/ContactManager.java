ecdd396e854224dd5cc6ce91ef0bbe198e4f0b8c
Peter Hardy <peterphardy@gmail.com>
2023-09-15 21:49:25 -0500

    Cleaner code corrections

--- Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/ContactManager.java
+++ Module 2 - Object-Oriented Programming/1. The World of Objects/Extra Practice/ContactManager.java
@@ -2,15 +2,14 @@ public class ContactManager {
     private Contact[] contacts;
     
     public ContactManager(Contact[] contacts) {
-        this.contacts = new Contact[contacts.length];
-        for (int i = 0; i < this.contacts.length; i++) {
-            this.contacts[i] = new Contact(contacts[i]);
+        contacts = new Contact[contacts.length];
+        for (int i = 0; i < contacts.length; i++) {
+            contacts[i] = new Contact(contacts[i]);
         }
     }
     
     public Contact getContact(int index) {
-        Contact copy = new Contact(this.contacts[index]);
-        return copy;
+        return new Contact(contacts[index]);
      }
      
 }
